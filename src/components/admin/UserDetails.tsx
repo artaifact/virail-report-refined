93fa7f44c181fdff57f6af28df83fa16abf90e70
herby <herby@herbys-macbook-pro.home>
2026-09-10 19:55:16 +0200

    userDetail

--- src/components/admin/UserDetails.tsx
+++ src/components/admin/UserDetails.tsx
@@ -310,7 +310,7 @@ export const UserDetails: React.FC<UserDetailsProps> = ({ userId, onBack, classN
             <CardHeader>
               <CardTitle className="flex items-center gap-2">
                 <User className="h-5 w-5" />
-                Informations personnelles
+                Informations personnelles test
               </CardTitle>
             </CardHeader>
             <CardContent className="space-y-4">
